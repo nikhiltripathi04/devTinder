74786906386eb89e4e80bb7364c626b048bf0f0a
nikhil tripathi <nikhiltr04@gmail.com>
2025-02-20 17:26:32 +0530

    exploring get post

---
+++ homework.md
@@ -0,0 +1,9 @@
+- initialize git
+- .gitignore 
+- create a remote repo on github
+- push all code to remote origin
+- play with routes and route extensions
+- order of routes matter a lot
+- install postman app and make a workspace/collection -> test API call
+- write logic to handle GET,, POST, PATCH, PUT, DELETE and test them on POSTMAN
+
--- src/app.js
+++ src/app.js
@@ -3,13 +3,20 @@ const express = require('express');
 
 const app = express();
 
+app.get('/user', (req, res) => {
+  res.send({firstName: "nikhil", lastName : "tripathi"});
+});
+
+app.post('/user', (req, res) => {
+  console.log("save data to DB");
+  res.send("data saved successfully to the db");
+});
+
 app.use("/test",(req, res) => {
   res.send("hello from the server");
 });
 
-app.use("/hello",(req, res) => {
-  res.send("hello from the server");
-});
+
 
 
 app.listen(7777, () => {
